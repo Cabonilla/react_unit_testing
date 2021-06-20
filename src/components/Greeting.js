10956b7de22e5fdd9cee0db27726da46aa9d9af1
cabonilla <christian.bonilla01@gmail.com>
2021-06-20 10:46:59 -0700

    Testing async code.

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import Async from './Async';
+import Output from './Output';
 
-const Greeting = ( ) => {
+const Greeting = () => {
 	const [changedText, setChangedText] = useState(false);
 
 	const changeTextHandler = () => {
@@ -8,13 +10,14 @@ const Greeting = ( ) => {
 	}
 
 	return (
-	<div>
-		<h2>Hello Laverne</h2>
-		{!changedText && <p>How are you?</p>  }
-		{/* <p>How are you?</p> */}
-		{changedText && <p>I'm fine Laverne.</p>}
-		<button onClick={changeTextHandler}>Change Text!</button>
-	</div>
+		<div>
+			<h2>Hello Laverne</h2>
+			{!changedText && <Output>How are you?</Output>}
+			{/* <p>How are you?</p> */}
+			{changedText && <Output>I'm fine Laverne.</Output>}
+			<button onClick={changeTextHandler}>Change Text!</button>
+			<Async/>
+		</div>
 	)
 }
 
