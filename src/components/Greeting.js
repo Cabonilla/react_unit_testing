b5f3f090df08a75a77393adad9d6e0928fe7feaa
cabonilla <christian.bonilla01@gmail.com>
2021-06-19 18:45:20 -0700

    Testing user interaction & state.

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+
 const Greeting = ( ) => {
+	const [changedText, setChangedText] = useState(false);
+
+	const changeTextHandler = () => {
+		setChangedText(true);
+	}
+
 	return (
 	<div>
 		<h2>Hello Laverne</h2>
-		<p>How are you?</p>
+		{!changedText && <p>How are you?</p>  }
+		{/* <p>How are you?</p> */}
+		{changedText && <p>I'm fine Laverne.</p>}
+		<button onClick={changeTextHandler}>Change Text!</button>
 	</div>
 	)
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,50 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
-test('renders Hello Laverne as a text', () => {
-	//Arrange
-	render(<Greeting />);
+describe('Greeting component', () => {
 
-	//Act
-	// ...nothing
+	test('renders Hello Laverne as a text', () => {
+		//Arrange
+		render(<Greeting />);
 
-	//Assert
-	const helloWorldElement = screen.getByText('Hello Laverne')
-	expect(helloWorldElement).toBeInTheDocument()
+		//Act
+		// ...nothing
+
+		//Assert
+		const helloWorldElement = screen.getByText('Hello Laverne')
+		expect(helloWorldElement).toBeInTheDocument()
+	})
+
+	test('renders How are you as a text', () => {
+		render(<Greeting/>)
+
+		const howAreYouElement = screen.getByText('How are you', {exact: false})
+		expect(howAreYouElement).toBeInTheDocument()
+	})
+	test('renders I\'m fine laverne if the button was clicked', () => {
+		//Arrange
+		render(<Greeting/>)
+
+		//Act(ion)
+		const buttonElement = screen.getByRole('button')
+		userEvent.click(buttonElement)
+
+		//Assert
+		const imFineElement = screen.getByText('I\'m fine Laverne', {exact: false})
+		expect(imFineElement).toBeInTheDocument()
+	})
+
+	test('does not render "How are you?" if the button was clicked', () => {
+		//Arrange
+		render(<Greeting/>)
+
+		//Act(ion)
+		const buttonElement = screen.getByRole('button')
+		userEvent.click(buttonElement)
+
+		//Assert
+		const imFineElement = screen.queryByText('How are you?', {exact: false})
+		expect(imFineElement).toBeNull();
+	})
 })
